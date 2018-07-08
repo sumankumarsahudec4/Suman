ec7cc95ec3af39a6c01ef50c20e4f9acd330313b
kajukajukajukajukajukaju <kajukajukajukajukajukaju@gmail.com>
2018-07-08 23:45:57 +0530

    First Time Commit Repo

---
+++ GitDemo_SUMAN/GitDemo_SUMAN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitDemo_SUMAN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
